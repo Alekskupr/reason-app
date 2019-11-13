800b151c44b9e3241722a0e29b27e93174d8c321
Alekskupr <leshakuprish@yandex.ru>
2019-11-13 11:03:44 +0300

    ver. 2

--- app.js
+++ app.js
@@ -32,7 +32,6 @@ const fileStoreOptions = {};
 app.use(
   session({
     store: new FileStore(fileStoreOptions),
-    // retries: 0,
     key: 'user_sid',
     secret: 'anything here',
     resave: false,
@@ -48,7 +47,7 @@ app.use(cookiesCleaner);
 app.use(
   methodOverride((req, res) => {
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-      // look in urlencoded POST bodies and delete it
+
       const method = req.body._method;
       delete req.body._method;
       return method;
@@ -70,13 +69,9 @@ app.use((req, res, next) => {
   next(createError(404));
 });
 
-// error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
